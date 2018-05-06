f7b818a1b0394c890ac2b7b8ccd5ce2a6f0c0495
bevkololi <kololibeverly@yahoo.com>
2018-05-06 21:56:52 +0300

    [chore] fix test auth

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,6 +1,6 @@
 import unittest
 import json
-from app import create_app, db
+from . import create_app, db
 
 class AuthTestCase(unittest.TestCase):
     """Test case for the authentication blueprint."""
